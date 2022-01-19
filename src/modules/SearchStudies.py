1aecd4da4995c4e049511e40bdc32dd02ec0f378
fwUniGit <2113700@uni-wuppertal.de>
2022-01-19 16:29:20 +0100

    All

--- src/modules/SearchStudies.py
+++ src/modules/SearchStudies.py
@@ -1,32 +1,93 @@
-import gensim
+import Similarities
+import ReferenceExtraction
+import os
+import logging
+import re
+import json
+
+# def getAbstracts():
+#     for file in ["3.pdf","7.pdf","9.pdf","11.pdf","15.pdf"]:
 
 
-def getPaperSimilarity(corpusset, queryDocument):
-    documents = corpusset
-    compare = queryDocument
 
-    stoplist = set('for a of the and to in'.split())
-    texts = [
-        [word for word in document.lower().split() if word not in stoplist]
-        for document in documents
-    ]
+def snowballing(starterSetPath, iterations):
+    starterSetAbstracts = []
+    referenceAbstracts = dict()
+    for file in starterSetPath:
+        starterSetAbstracts.append(
+            re.sub("<\/jats.*?>|<jats.*?>", "", ReferenceExtraction.getAbstractByPdf(file)['abstract']))
+    for file in starterSetPath:
+        referenceAbstracts.update(ReferenceExtraction.getReferencedPapers(file))
 
-    dictionary = gensim.corpora.Dictionary(texts)
-    corpus = [dictionary.doc2bow(text) for text in texts]
+    for paper in referenceAbstracts:
+        if referenceAbstracts[paper]["references"] != "None":
+            referenceList = []
+            for reference in referenceAbstracts[paper]["references"]:
+                referenceList.append(reference.get("DOI"))
+            referenceList = [x for x in referenceList if x is not None]
+            referenceAbstracts[paper]["references"] = referenceList
 
-    feature_cnt = len(dictionary.token2id)
-    print(feature_cnt)
+    filtered = {k:v
+          for k, v in referenceAbstracts.items()
+          if v["abstract"] != 'None'}
+    referenceAbstracts = filtered
+    for key in referenceAbstracts:
+        referenceAbstracts[key]["abstract"] = re.sub("<\/jats.*?>|<jats.*?>", "", referenceAbstracts[key]["abstract"])
+    corpus_set = starterSetAbstracts
+    query_set = referenceAbstracts
+    result_set = {}
+    new_set = getSimilarReferences(corpus_set,query_set,0.8)
+    result_set.update(new_set.copy())
+    print("First Iteration done")
+    print(result_set)
+    i=0
+    while i < iterations:
+        referenceAbstracts = dict()
+        for paperKey in new_set:
+            corpus_set.append(new_set[paperKey]['abstract'])
+            if new_set[paperKey]['references'] == 'None' and "crossref" not in paperKey:
+                referenceAbstracts.update(ReferenceExtraction.getReferencedPapers(paperKey))
+            elif new_set[paperKey]['references'] != 'None':
+                for reference in new_set[paperKey]['references']:
+                    referenceAbstracts[reference] = ReferenceExtraction.getAbstractFromDoi(reference)
 
-    lsi = gensim.models.LsiModel(corpus, id2word=dictionary)
+        for paper in referenceAbstracts:
+            if referenceAbstracts[paper]["references"] != "None":
+                referenceList = []
+                for reference in referenceAbstracts[paper]["references"]:
+                    referenceList.append(reference.get("DOI"))
+                referenceList = [x for x in referenceList if x is not None]
+                referenceAbstracts[paper]["references"] = referenceList
 
-    vec_bow = dictionary.doc2bow(compare.lower().split())
-    vec_lsi = lsi[vec_bow]  # convert the query to LSI space
-    print(vec_lsi)
+        filtered = {k:v
+              for k, v in referenceAbstracts.items()
+              if v["abstract"] != 'None'}
+        referenceAbstracts = filtered
+        for key in referenceAbstracts:
+            referenceAbstracts[key]["abstract"] = re.sub("<\/jats.*?>|<jats.*?>", "",referenceAbstracts[key]["abstract"])
+        query_set = referenceAbstracts
+        new_set.clear()
+        new_set = getSimilarReferences(corpus_set,query_set,0.8)
+        result_set.update(new_set.copy())
+        print("-----------------------------NextIteration done-------------------------------")
+        print(new_set)
+        print(len(new_set))
+        i += 1
+    return json.dumps(result_set)
 
-    index = gensim.similarities.SparseMatrixSimilarity(lsi[corpus], num_features=feature_cnt)
 
-    sim = index[vec_lsi]
+def getSimilarReferences(corpus_set, query_set, min_similarity):
+    similarities = Similarities.sBERT_querys(corpus_set, query_set)
+    new_set = {}
+    for paper in similarities:
+        for score in similarities[paper][0]:
+            if score["score"] > min_similarity and paper not in new_set:
+                new_set[paper] = query_set[paper]
+                new_set[paper]["similarity"] = f"{score['score']}, similar to corpus_set Papers"
+    return new_set
 
-    for i in range(len(sim)):
-        print('keyword is similar to text%d: %.2f' % (i + 1, sim[i]))
-
+result = snowballing(["9.pdf","15.pdf"],1)
+print(result)
+print(len(result))
+with open("test.json", "w") as f:
+    f.write(json.dumps(json.loads(result), indent=4, sort_keys=True))
