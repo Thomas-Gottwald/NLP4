6732437e7133d9b98f91e8731c2b848d0af8600c
JPOonGIT <94445942+JPOonGIT@users.noreply.github.com>
2022-02-14 08:38:07 +0100

    New Files

--- src/modules/PaperSelection.py
+++ src/modules/PaperSelection.py
@@ -3,31 +3,41 @@ import pandas as pd
 from plotly.subplots import make_subplots as ms
 
 
-def paper_importance(text=[], keywords=[]):
+def paper_importance(text=[], keywords=[], rank_title=[]):
 
     model = SentenceTransformer('allenai-specter')
-
     keyword_corpus = model.encode(keywords, convert_to_tensor=True)
     match_set = {}
     text_corpus = model.encode(text, convert_to_tensor=True)
     match_set = util.semantic_search(text_corpus, keyword_corpus, top_k=len(keywords))
 
-    rank_title = []
-    for i in range(0, len(text)):
-        rank_title.append('Text_' + str(i))
+    if len(rank_title) == len(text):
+        df = pd.DataFrame(index=rank_title, columns=keywords)
 
-    df = pd.DataFrame(index=rank_title, columns=keywords)
+        for i, match in enumerate(match_set):
 
-    for i, match in enumerate(match_set):
+            for entry in match:
+                df.iloc[i, entry['corpus_id']] = entry['score']
+        print(df)
+        return df
 
-        for entry in match:
-            df.iloc[i, entry['corpus_id']] = entry['score']
-    print(df)
-    return df
+    else:
+        print('Not enough Titles for the Papers names are replaced by Numbers')
+        rank_title_num = list()
+        for i in range(0, len(text)):
+            rank_title_num.append('Text_' + str(i))
+
+        df = pd.DataFrame(index=rank_title_num, columns=keywords)
+
+        for i, match in enumerate(match_set):
+
+            for entry in match:
+                df.iloc[i, entry['corpus_id']] = entry['score']
+        print(df)
+        return df
 
 
 def plot_paper_selection(df=pd.DataFrame()):
-
     fig = ms(rows=1, cols=1, x_title='Keywords', y_title='Similarity')
     for i, name in enumerate(df.index):
         fig.add_scatter(x=df.columns, y=df.iloc[i, :], mode="lines + markers", marker=dict(size=10),
